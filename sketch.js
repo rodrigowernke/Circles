4af53bc5a916bdbf4bcd211feb207b20e012632f
Rodrigo Pereira <rodrigo.pereira95@gmail.com>
2018-05-07 21:17:14 -0300

    Atualização do sketch

--- sketch.js
+++ sketch.js
@@ -1,33 +1,32 @@
 let circles = [];
 
-function spawn(){
+function spawn() {
   circles = []
 
   for (var i = 0; i < 100; i++) {
-    let col = color(255,0,0);
-    let d = new Circle(random(width),random(height),col);
+    let col = color(255, 0, 0);
+    let d = new Circle(random(width), random(height), col);
     circles.push(d);
   }
 }
 
 function setup() {
-
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
   background(0);
 
   spawn();
 }
 
 function mousePressed() {
-    fullscreen(true);
-    resizeCanvas(displayWidth, displayHeight);
-    spawn();
+  fullscreen(true);
+  resizeCanvas(displayWidth, displayHeight);
+  spawn();
 }
 
 function draw() {
   background(0);
 
-  for(let i = 0; i < circles.length;i++){
+  for (let i = 0; i < circles.length; i++) {
     circles[i].update();
     circles[i].show();
   }
